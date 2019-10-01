f84170deb980517abf8d0926b355c5485a09f5bb
dominick-designs <dominick@dominickdesigns.space>
2019-09-30 23:00:10 -0400

    Implement Authentication Middleware Function

--- routes/users.js
+++ routes/users.js
@@ -32,6 +32,48 @@ const asyncHandler = (cb) => async (req, res, next) => {
     }
 };
 
+
+
+
+const authenticateUser = (req, res, next) => {
+    let message = null;
+
+    const credentials = auth(req);
+
+    if (credentials) {
+        const user = models.User.findAll({
+            limit: 1,
+            where: {
+                emailAddress: credentials.name,
+                password: credentials.pass,
+            }
+        });
+        console.log("this is user.password:", credentials.name);
+        if (user) {
+            // const authenticated = bcrypt.compareSync(credentials.pass, user.password);
+
+            // if (authenticated) {
+            //     console.log(`Authentication successful for username: ${user.emailAddress}`);
+            //     req.currentUser = user;
+            // } else {
+            //     message = `Authentication failure for username: ${credentials.name}`;
+            // }
+            console.log(user.emailAddress)
+        } else {
+            message = `User not found for username: ${credentials.name}`;
+        }
+    } else {
+        message = 'Auth header not found';
+    }
+
+    if (message) {
+        console.warn(message);
+        res.status(401).json({ message: `Access Denied` });
+    } else {
+        next();
+    }
+};
+
 /** ADAPTED FROM
  * https://sequelize.readthedocs.io/en/1.7.0/articles/express/ 
  * https://gist.github.com/vapurrmaid/a111bf3fc0224751cb2f76532aac2465
@@ -50,39 +92,34 @@ router.get('/all', asyncHandler(async (req, res) => {
         });
 }));
 
-router.get('/', asyncHandler(async (req, res) => {
-    // Basic function to validate credentials for example
-    const check = (name, pass) => {
-        var valid = true;
-        // Simple method to prevent short-circut and use timing-safe compare
-        valid = compare(name, user.name) && valid;
-        valid = compare(pass, user.pass) && valid;
-        return valid;
-    };
-
-    const user = auth(req);
-    // Check credentials
-    if (!user || !check(user.name, user.pass)) {
-        res.statusCode = 401;
-        res.setHeader('WWW-Authenticate', 'Basic realm="example"');
-        res.end('Access denied. Your credentials are not valid.');
-    } else {
-        await models.User.findAll({
-            where: {
-                emailAddress: user.name,
-                password: user.pass,
-            }
-        })
-            .then(user => {
-                if (user) {
-                    res.json({ user: user });
-                }
-            });
-        res.end('Access granted');
-    }
+/** Set up permissions to require users to be signed in
+ * Add a middleware function that attempts to get the user credentials from the Authorization header set on the request.
+ * You can use the basic-auth npm package to parse the Authorization header into the user's credentials.
+ * The user's credentials will contain two values: a name value—the user's email address—and a pass value—the user's password (in clear text).
+ * Use the user's email address to attempt to retrieve the user from the database.
+ * If a user was found for the provided email address, then check that user's stored hashed password against the clear text password given using bcryptjs.
+ * If the password comparison succeeds, then set the user on the request so that each following middleware function has access to it.
+ * If the password comparison fails, then return a 401 status code to the user.
+ * 
+ * Use this middleware in the following routes:
+GET /api/users
+POST /api/courses
+PUT /api/courses/:id
+DELETE /api/courses/:id
+ */
+router.get('/', authenticateUser, (req, res) => {
+    const user = req.currentUser;
+    res.send('ok from line 110').end();
+    // res.json({
+    //     name: user.name,
+    //     username: user.username,
+    // });
 
 
-}));
+
+
+
+});
 
 
 //POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
@@ -90,7 +127,7 @@ router.post('/', asyncHandler(async (req, res) => {
     //bcrypt hash function adapted from https://medium.com/@mridu.sh92/a-quick-guide-for-authentication-using-bcrypt-on-express-nodejs-1d8791bb418f
     const saltRounds = 10;
     if (req.body.firstName && req.body.lastName && req.body.emailAddress && req.body.password) {
-        bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+        bcrypt.hashSync(req.body.password, saltRounds, async (err, hash) => {
             // Store hash in your password DB.
             await models.User.create({
                 firstName: req.body.firstName,
@@ -152,8 +189,6 @@ router.put('/:id', asyncHandler(async (req, res) => {
 router.delete('/:id', asyncHandler(async (req, res) => {
     const user = await models.User.findByPk(req.params.id);
     if (user) {
-
-
         await models.User.destroy({
             where: {
                 id: req.params.id
@@ -170,13 +205,3 @@ router.delete('/:id', asyncHandler(async (req, res) => {
 }));
 
 module.exports = router;
-
-/*
-                json({
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    emailAddress: req.body.emailAddress,
-                    password: req.body.password
-                });
-
-                */
